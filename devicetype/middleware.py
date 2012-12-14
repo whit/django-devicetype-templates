4a00439e32aa6b6d6add708a7d8b6fc009635fb6
Vitek Pliska <whit@jizak.cz>
2012-12-14 13:55:41 +0100

    Returns redirect in proces_request: command not found, refactoring

--- devicetype/middleware.py
+++ devicetype/middleware.py
@@ -2,6 +2,7 @@ import logging
 import time
 
 from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.utils.http import cookie_date
 
 from devicetype import conf
@@ -17,19 +18,21 @@ class DeviceTypeMiddleware(object):
 
     def process_request(self, request):
 
-        # force switch link
+        # force switch device type, set cookie and return redirect
         if 'devicetype' in request.GET and request.GET['devicetype'] in conf.DEVICE_TYPES:
-            request.devicetype = request.GET['devicetype']
-            return None
+            response = HttpResponseRedirect(request.META['PATH_INFO'])
+            response.set_cookie('devicetype', request.GET['devicetype'], domain=settings.SESSION_COOKIE_DOMAIN,
+                                max_age=conf.DEVICE_TYPE_COOKIE_MAXAGE, expires=self._get_expires())
 
-        # test saved cookie
+            return response
+
+        # set type from saved cookie and return
         cookie = request.COOKIES.get('devicetype', None)
-
         if cookie in conf.DEVICE_TYPES:
             request.devicetype = cookie
             return None
 
-        # test for mobile browser
+        # check type by UA string, if present
         if not 'HTTP_USER_AGENT' in request.META:
             return None
 
@@ -46,6 +49,7 @@ class DeviceTypeMiddleware(object):
         orig_template_name = response.template_name
         new_template_name = []
 
+        # add templates only if prefix is not empty
         if request.devicetype in conf.DEVICE_TYPES and conf.TEMPLATE_PREFIX[request.devicetype]:
             if isinstance(orig_template_name, (list, tuple)):
                 orig_template_name = list(orig_template_name)
@@ -57,29 +61,12 @@ class DeviceTypeMiddleware(object):
             new_template_name.extend(orig_template_name)
             response.template_name = new_template_name
 
-        # set cookie to identify the browser as mobile
-        expires_time = time.time() + conf.DEVICE_TYPE_COOKIE_MAXAGE
-        expires = cookie_date(expires_time)
         response.set_cookie('devicetype', request.devicetype, domain=settings.SESSION_COOKIE_DOMAIN,
-                            max_age=conf.DEVICE_TYPE_COOKIE_MAXAGE, expires=expires)
+                            max_age=conf.DEVICE_TYPE_COOKIE_MAXAGE, expires=self._get_expires())
 
         return response
 
-#    def process_response(self, request, response):
-#
-#        # set cookie to identify the browser as mobile
-#        expires_time = time.time() + DEFAULT_COOKIE_MAX_AGE
-#        expires = cookie_date(expires_time)
-#        response.set_cookie('devicetype', request.devicetype, domain=settings.SESSION_COOKIE_DOMAIN,
-#            max_age=DEFAULT_COOKIE_MAX_AGE, expires=expires)
-#
-#        # remove force get parameter
-#        if 'devicetype' in request.GET:
-#            return HttpResponseRedirect(request.META['PATH_INFO'])
-#
-#        return response
+    def _get_expires(self):
 
-
-class RedirectMiddleware:
-    # TODO: mobile version(s) on other place...
-    pass
+        expires_time = time.time() + conf.DEVICE_TYPE_COOKIE_MAXAGE
+        return cookie_date(expires_time)
